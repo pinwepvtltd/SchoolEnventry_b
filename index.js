33f759314adbdacc6e89a7699c2c1331429375bf
PinWeb Private Limited <pinweball@gmail.com>
2026-04-09 15:49:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use('/api/product', productRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/sale', saleRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
